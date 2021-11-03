3e955e0d6e947f4f9ddceca47a0798041ada4cc2
takashiidobe <idobetakashi@gmail.com>
2021-11-03 08:17:55 -0500

    only test out broken tier 1s + 2s

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-use test_bin::*;
-
-#[async_std::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (filename, pager) = arg_parser()?;
-    read_file(filename, pager.finish()).await
+fn main() {
+    println!("hi");
 }
