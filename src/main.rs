96a09d680cb304aa01c32f289f7b1983eea03b0c
takashiidobe <idobetakashi@gmail.com>
2021-11-04 09:02:12 -0500

    try to compile without std

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello World");
+#![no_std]
+#![no_main]
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[link(name = "c")]
+extern "C" {
+    fn write(fd: i32, buf: *const i8, count: usize) -> isize;
+}
+
+#[no_mangle]
+pub extern "C" fn main() -> isize {
+    unsafe { write(1, b"Hello, World!\n" as *const u8 as *const i8, 14) };
+    0
 }
