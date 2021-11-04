3976fba3846fd6cb57f98dfccea61bdd084f3e54
takashiidobe <idobetakashi@gmail.com>
2021-11-03 20:21:34 -0500

    go back to std to work on windows

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,3 @@
-#![no_std]
-#![no_main]
-
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
-
-#[link(name = "c")]
-extern "C" {
-    fn write(fd: i32, buf: *const i8, count: usize) -> isize;
-}
-
-#[no_mangle]
-pub extern "C" fn main() -> isize {
-    unsafe { write(1, b"Hello, World!\n" as *const u8 as *const i8, 14) };
-    0
+fn main() {
+    println!("Hello World");
 }
